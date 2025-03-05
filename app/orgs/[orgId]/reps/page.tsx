07a6a67d4b49be2aea13968f5312ab8bd420f3a1
kevin.thizy <kevin.thizy@outlook.fr>
2025-03-05 23:30:14 +0100

    ♻️(reps) New structure and add standing

--- app/orgs/[orgId]/reps/page.tsx
+++ app/orgs/[orgId]/reps/page.tsx
@@ -10,6 +10,7 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { PlayerReputations } from "@/types/reputations";
 
 export default async function ReputationPage({
   params,
@@ -49,7 +50,7 @@ export default async function ReputationPage({
       users: {
         avatar: string;
         name: string;
-        reputations: { [faction: string]: { level: number; name: string } };
+        reputations?: PlayerReputations;
       }[];
     }>({ users: 1, name: 1 })
     .toArray();
@@ -62,27 +63,111 @@ export default async function ReputationPage({
 
   const organisation = aggregation[0];
 
-  const usersByFactionLevel = factions.reduce(
-    (
-      acc: { [faction: string]: { [level: string]: { name: string }[] } },
-      faction,
-    ) => {
-      acc[faction.name] = faction.levels.reduce(
-        (levelAcc: { [level: string]: { name: string }[] }, level) => {
-          levelAcc[level.name] = organisation.users.filter((user) => {
-            const userReputation = user.reputations?.[faction.name];
-            return userReputation && userReputation.level === level.level;
-          });
+  const membersBreakdown: Record<
+    string,
+    {
+      name: string;
+      standings: Record<
+        string,
+        { name: string; count: number; members: string[] }
+      >;
+      careers: Record<
+        string,
+        {
+          name: string;
+          levels: Record<
+            string,
+            { name: string; count: number; members: string[] }
+          >;
+        }
+      >;
+    }
+  > = {};
 
-          return levelAcc;
-        },
-        {},
-      );
+  organisation.users.forEach((user) => {
+    if (!user.reputations) {
+      return;
+    }
 
-      return acc;
-    },
-    {},
-  );
+    factions.forEach((faction) => {
+      const factionName = faction.name;
+
+      if (!membersBreakdown[factionName]) {
+        membersBreakdown[factionName] = {
+          name: factionName,
+          standings: {},
+          careers: {},
+        };
+      }
+
+      if (!user.reputations?.[factionName]) {
+        return;
+      }
+
+      faction.standings.forEach((standing) => {
+        if (!membersBreakdown[factionName].standings[standing]) {
+          membersBreakdown[factionName].standings[standing] = {
+            name: standing,
+            count: 0,
+            members: [],
+          };
+        }
+
+        if (!user.reputations?.[factionName]) {
+          return;
+        }
+
+        if (user.reputations[factionName].standing === standing) {
+          membersBreakdown[factionName].standings[standing].count++;
+          membersBreakdown[factionName].standings[standing].members.push(
+            user.name,
+          );
+        }
+      });
+
+      faction.careers.forEach((career) => {
+        const careerName = career.name;
+
+        if (!membersBreakdown[factionName].careers[careerName]) {
+          membersBreakdown[factionName].careers[careerName] = {
+            name: careerName,
+            levels: {},
+          };
+        }
+
+        career.levels.forEach((level) => {
+          const levelName = level.name;
+
+          if (
+            !membersBreakdown[factionName].careers[careerName].levels[levelName]
+          ) {
+            membersBreakdown[factionName].careers[careerName].levels[
+              levelName
+            ] = {
+              name: levelName,
+              count: 0,
+              members: [],
+            };
+          }
+
+          if (!user.reputations?.[factionName]?.careers[careerName]) {
+            return;
+          }
+
+          if (
+            user.reputations[factionName].careers[careerName].level.name ===
+            levelName
+          ) {
+            membersBreakdown[factionName].careers[careerName].levels[levelName]
+              .count++;
+            membersBreakdown[factionName].careers[careerName].levels[
+              levelName
+            ].members.push(user.name);
+          }
+        });
+      });
+    });
+  });
 
   return (
     <div className="m-2 p-6 max-w-4xl mx-auto bg-white rounded-xl shadow-md space-y-4">
@@ -112,22 +197,38 @@ export default async function ReputationPage({
       <p>{t("description")}</p>
 
       <div className="space-y-4">
-        {Object.entries(usersByFactionLevel).map(([factionName, levels]) => (
-          <Card key={factionName} className="p-4">
-            <CardTitle>{factionName}</CardTitle>
+        {Object.values(membersBreakdown).map((faction) => (
+          <Card key={faction.name} className="p-4">
+            <CardTitle>{faction.name}</CardTitle>
             <CardContent>
-              {Object.entries(levels).map(([levelName, users]) => (
-                <div key={levelName}>
-                  <h3 className="text-lg font-medium">{levelName}</h3>
-                  <ul className="list-disc list-inside">
-                    {users.map((user, index) => (
-                      <li key={index} className="text-sm">
-                        {user.name ?? "Unnamed user"}
-                      </li>
-                    ))}
-                  </ul>
+              <div className="">
+                <h2 className="font-bold">Réputations</h2>
+                <div className="flex gap-2 flex-wrap">
+                  {Object.values(faction.standings).map((standing) => (
+                    <div key={standing.name} className="flex items-center">
+                      <span className="mr-2">{standing.name}</span>
+                      <span>{standing.count}</span>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
+
+              <h2 className="font-bold">Carrières</h2>
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+                {Object.values(faction.careers).map((career) => (
+                  <div key={career.name}>
+                    <h3 className="font-semibold">{career.name}</h3>
+                    <div className="">
+                      {Object.values(career.levels).map((level) => (
+                        <div key={level.name} className="flex items-center">
+                          <span className="mr-2">{level.name}</span>
+                          <span>{level.count}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </CardContent>
           </Card>
         ))}
